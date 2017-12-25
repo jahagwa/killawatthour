bcc11a4268e331c643031fc9527cb6e9bdea2dcc
Anthony Mellinger <jahagwa@gmail.com>
2017-12-25 16:59:20 -0700

    shortenedscript

--- practice.py
+++ practice.py
@@ -1,11 +1,6 @@
-with open('/proc/uptime') as f:
-    read_data = f.read()
-
-uptime = float(read_data.split(" ")[0]) / 3600
-
-
 with open('/sys/class/powercap/intel-rapl:0/energy_uj') as d:
     read_data = d.read()
-uj = int(read_data) *10**-6
 
-print(uj/uptime * .07)
+uj = float(read_data) / 3600000000000
+
+print(uj)
